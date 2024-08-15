797528e28cce13872901ced4a38ef0dffa9c1ac7
ralekeo <ralekeo@gmail.com>
2024-08-15 12:00:40 +0100

    modified:   src/App.js 	new file:   src/ft.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+function Apps(){
+  return <h1>Hello React!</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Apps />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
